ca9597899bc38f505c27b799f2f6e2f3eef3e6e1
Mackenzie Lamour <maclamour@gmail.com>
2023-10-08 10:54:11 -0400

    updated resume download

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -1,29 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 
 export default function Resume() {
-  const [isDownloading, setIsDownloading] = useState(false);
-
-  function handleDownloadClick() {
-    setIsDownloading(true);
-    // Simulate a delay (optional)
-    setTimeout(() => {
-      setIsDownloading(false);
-    }, 2000); // 2 seconds (adjust as needed)
-
-    // Use the actual download link
+  useEffect(() => {
+    // Use the actual download link on page load
     window.location.href =
       'https://drive.google.com/uc?export=download&id=16LndCVBEPE2fdesz_QUwV7ADlCdATij2';
-  }
+  }, []);
 
   return (
     <div>
-      {isDownloading ? (
-        <p>Downloading resume...</p>
-      ) : (
-        <a href="#" onClick={handleDownloadClick}>
-          Download Resume
-        </a>
-      )}
+      <p>Downloading resume...</p>
     </div>
   );
 }
+
