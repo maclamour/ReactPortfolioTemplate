b2fca8cd220144c1a2e57b3fbe8a6f52a4f2f158
Mackenzie Lamour <maclamour@gmail.com>
2023-10-08 10:37:21 -0400

    updated resume download

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import { Box } from '@mui/material';
 
 export default function Resume() {
-  function resumeText() {
-    return (
-      <>
-        <p><span className={Style.green}>$</span> cat resume</p>
-        <p>Downloading resume...</p>
-        <a
-          href="https://drive.google.com/uc?export=download&id=16LndCVBEPE2fdesz_QUwV7ADlCdATij2"
-          className={Style.resumeLink}
-          download
-        >
-          Download Resume
-        </a>
-      </>
-    );
-  }
-
   return (
-    <Box display={'flex'} flexDirection={'column'} alignItems={'center'} mt={'3rem'}>
-      <Terminal text={resumeText()} />
-    </Box>
+    <div>
+      <p>Downloading resume...</p>
+      <a
+        href="https://drive.google.com/uc?export=download&id=16LndCVBEPE2fdesz_QUwV7ADlCdATij2"
+        download
+      >
+        Download Resume
+      </a>
+    </div>
   );
 }
